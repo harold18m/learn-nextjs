90c973ecd6a3d5005592cd3ee83bdf7201ec8bbc
harold18m <harold.medrano@tecsup.edu.pe>
2024-12-19 19:50:47 -0500

    Chapter 6

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col p-6">
+            <p>Page</p>
+            <h1>Invoices</h1>
+        </main>
+    )
+}
